b826236667c33c811a0378791b5893d54feb5e16
Sumeet Shirgure <sshirgur@usc.edu>
2023-09-19 14:26:24 -0700

    Add tests.

--- tests/tree/hull_merge_test.cc
+++ tests/tree/hull_merge_test.cc
@@ -95,10 +95,60 @@ int test_upper() {
 }
 
 
+int test_upper_2() {
+  using upper_hull_t = MergeableUpperHull<int>;
+  upper_hull_t left_hull, right_hull;
+
+  for(int i = 9; i >= 5; i--) {
+    upper_hull_t::join(left_hull, left_hull, upper_hull_t(
+          LineSegment(
+            Point<int>{-i-1, 9-(i-7)*(i-7)},
+            Point<int>{-i, 9 -(i-8)*(i-8) }
+            )
+          )
+        );
+  }
+  for(int i = 4; i >= 0; i--) {
+    upper_hull_t::join(right_hull, right_hull, upper_hull_t(
+          LineSegment(
+            Point<int>{2-i, 23-(i-2)*(i-2)},
+            Point<int>{-i+3, 23-(i-3)*(i-3)}
+            )
+          )
+        );
+  }
+
+  for(auto seg: left_hull) {
+    cerr << "[" << to_string(seg.u) << " -- " << to_string(seg.v) << "]" << endl;
+  }
+
+  for(auto seg: right_hull) {
+    cerr << "[" << to_string(seg.u) << " -- " << to_string(seg.v) << "]" << endl;
+  }
+
+  cout << is_concave(left_hull) << endl;
+  cout << is_concave(right_hull) << endl;
+
+  upper_hull_t merged, left_residue, right_residue;
+
+
+  auto bridge = merge_upper_hulls<>(merged, left_hull, right_hull, left_residue, right_residue);
+
+  for(auto it: merged) cerr << to_string(it) << " ";cerr << endl;
+  for(auto it: left_residue) cerr << to_string(it) << " ";cerr << endl;
+  for(auto it: right_residue) cerr << to_string(it) << " ";cerr << endl;
+
+  cout << is_concave(merged) << endl;
+
+  return 0;
+}
+
+
 int main() {
 
   test_lower();
   test_upper();
+  test_upper_2();
 
   return 0;
 }
